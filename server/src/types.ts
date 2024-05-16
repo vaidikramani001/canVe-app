f2f4f765fb9044efb2972537497995f65d217f16
vaidik <146804512+vaidikramani001@users.noreply.github.com>
2024-05-16 16:47:22 +0530

    Update types.ts

--- server/src/types.ts
+++ server/src/types.ts
@@ -19,10 +19,8 @@ import { Request, Response } from "express";
 import { SessionData } from "express-session";
 import { PrismaClient } from '@prisma/client';
 import { AccessTokenData } from "./services";
-import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 
 export type MyContext = {
-    em: EntityManager<IDatabaseDriver<Connection>>;
     prisma: PrismaClient;
     req: Request & { session: SessionData & { userId: number } };
     res: Response;
@@ -30,9 +28,4 @@ export type MyContext = {
     user: AccessTokenData | null;
 };
 
-export type Role = "ADMIN" | "USER";
-
-export type Permission = POST_PERMISSIONS | EVENT_PERMISSIONS | USER_PERMISSIONS;
-type POST_PERMISSIONS = "post_create" | "post_read" | "post_write" | "post_delete";
-type EVENT_PERMISSIONS = "event_create" | "event_read" | "event_write" | "event_delete";
-type USER_PERMISSIONS = "user_create" | "user_read" | "user_write" | "user_delete";
+export type Role = "ADMIN" | "USER"; 
